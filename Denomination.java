7c9a7c1027caa983f3c5643f56a9470ef332ba74
Arvind Sundar <arvindsundar1996@gmail.com>
2017-07-03 09:33:50 +0530

    Add files via upload

---
+++ Denomination.java
@@ -0,0 +1,25 @@
+package guvi;
+import java.util.Scanner;
+public class Denomination {
+	public static void main(String[] args){
+		Scanner in=new Scanner(System.in);
+		int a;
+		System.out.println("Enter the amount to be denominated");
+		a=in.nextInt();
+		int fi,fiv,hu,hun,fif,fift,te,ten,on,one;
+		fi=a/500;
+		fiv=a%500;
+		hu=fiv/100;
+		hun=fiv%100;
+		fif=hun/50;
+		fift=hun%50;
+		te=fift/10;
+		ten=fift%10;
+		on=ten/1;
+		one=ten%10;
+		int sum=fi+hu+fif+te+on;
+		System.out.println(" 500rs="+fi+" 100rs="+hu+" 50rs="+fif+" 10rs="+te+" 1rupee="+on);
+		System.out.println("Total no.of currency notes="+sum);
+	}
+
+}
