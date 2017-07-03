ae51edf22cdc8d63c384fc685a7b865802765649
Arvind Sundar <arvindsundar1996@gmail.com>
2017-07-03 15:48:00 +0530

    Add files via upload

---
+++ AsciiValues.java
@@ -0,0 +1,13 @@
+package guvi;
+public class AsciiValues {
+	public static void main(String[] args){
+		int a;
+		char b;
+		for(int i=0;i<=255;i++){
+			b=(char) i;
+			System.out.println(b);
+			
+		}
+	}
+
+}
