8800486ffd4d1b4411936a04c39596bb00811a13
Arvind Sundar <arvindsundar1996@gmail.com>
2017-07-17 15:32:07 +0530

    Add files via upload

--- ReverseWord.java
+++ ReverseWord.java
@@ -3,8 +3,17 @@ public class ReverseWord {
 	public static void main(String[] args){
 		String ip;
 		Scanner in=new Scanner(System.in);
+		System.out.println("Enter the String");
 		ip=in.nextLine();
 		String[] ip1=ip.split(" ");
-			System.out.println(ip1[1]+" "+ip1[0]);
+		for(int i=0;i<ip1.length;i++){
+			String s1=new StringBuffer(ip1[i]).reverse().toString();
+			ip1[i]=s1;
+			i++;
+		}
+		for(int i=0;i<ip1.length;i++){
+			System.out.print(" "+ip1[i]);
+		}
+			
 	}
 }
