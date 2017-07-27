39b981368486e58e0d235304c3df5a2c788fbe04
Arvind Sundar <arvindsundar1996@gmail.com>
2017-07-27 15:42:56 +0530

    Add files via upload

---
+++ Player88.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Player88 {
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		int n=in.nextInt();
+		for(int i=1;i<=n;i++){
+			System.out.println("");
+			for(int j=1;j<=i;j++){
+				System.out.print(j+" ");
+			}
+		}
+	}
+
+}
