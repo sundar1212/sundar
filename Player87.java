51ea3e630275590e73e0d9774323e858517e4dee
Arvind Sundar <arvindsundar1996@gmail.com>
2017-07-27 12:31:32 +0530

    Add files via upload

---
+++ Player87.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Player87 {
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		int n=in.nextInt();
+		int a=in.nextInt();
+		int b=in.nextInt();
+		int[] arr=new int[n];
+		arr[a]=b;
+		System.out.println("The array element in index "+a+" is="+ b);
+		
+	}
+
+}
