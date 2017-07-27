bdf2d485649aac4fe94536933ef0e12b3854f37c
Arvind Sundar <arvindsundar1996@gmail.com>
2017-07-27 15:57:18 +0530

    Add files via upload

---
+++ Player97.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Player97 {
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		int n=in.nextInt();
+		int sum=0;
+		for(int i=1;i<=n;i++){
+			if(i%2!=0){
+				sum=sum+i;
+			}
+			
+		}
+		System.out.println(sum);
+	}
+}
